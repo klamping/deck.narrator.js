f2550a41eb5a419f4c879a4d47afe2254420d54b
Kevin Lamping <klamping@gmail.com>
2013-05-07 23:05:11 -0500

    first commit

---
+++ deck.narrator.js
@@ -0,0 +1,17 @@
+/*!
+Deck JS - deck.narrator
+Copyright (c) 2013 Kevin Lamping
+Dual licensed under the MIT license and GPL license.
+https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
+https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
+*/
+
+/*
+This module adds a audio narration to slides
+*/
+(function($, deck, undefined) {
+	var $d = $(document),
+	
+	
+})(jQuery, 'deck');
+
